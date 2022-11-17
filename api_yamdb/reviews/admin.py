1ca21f0ed84368255bf9b6755e418352af81f576
Alprika <umbrella-tree@mail.ru>
2022-11-17 06:26:20 +0300

    Создание структуры проекта

---
+++ api_yamdb/reviews/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+
+from reviews.models import (Category,
+                            Comment,
+                            Genre,
+                            Review,
+                            Title)
+
+admin.site.register([
+    Category,
+    Comment,
+    Genre,
+    Review,
+    Title
+])
